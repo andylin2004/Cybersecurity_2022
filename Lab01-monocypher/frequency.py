c0907b0502f7defb8eeafcccbc33e9f02d5288da
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-03-02 15:26:10 -0500

    p1.1 done

--- Lab01-monocypher/frequency.py
+++ Lab01-monocypher/frequency.py
@@ -3,11 +3,17 @@
 import sys
 
 charDistribution = {}
+total = 0
+
+for i in range(65,91):
+    charDistribution[chr(i)] = 0
+
 file = open(sys.argv[1], "r")
 for line in file:
-    for char in line:
-        if char not in charDistribution:
-            charDistribution[char] = 0
-        charDistribution[char] += 1
+    for char in line.upper():
+        if char in charDistribution:
+            charDistribution[char] += 1
+            total += 1
 
-print(charDistribution)
+for i in charDistribution:
+    print(i+": "+str(charDistribution[i]/total))
