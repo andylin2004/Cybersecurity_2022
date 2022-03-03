36248691ffa9b8c7daedde68e5862b108efac843
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-03-03 14:40:12 -0500

    tried to fix the decode algo more

--- Lab01-monocypher/decode.py
+++ Lab01-monocypher/decode.py
@@ -14,9 +14,9 @@ def decode(fileName: str):
     temp = frequencyInput[chr(65)]
 
     for i in range(26):
-        for i in range(65, 90):
-            anotherTemp = frequencyInput[chr(i+1)]
-            frequencyInput[chr(i+1)] = temp
+        for v in range(65, 90):
+            anotherTemp = frequencyInput[chr(v+1)]
+            frequencyInput[chr(v+1)] = temp
             temp = anotherTemp
 
         frequencyInput[chr(65)] = temp
@@ -34,6 +34,13 @@ def decode(fileName: str):
         frequencyInput[chr(90-i)] = temp
 
     for i in range(26):
+        for v in range(65, 90):
+            anotherTemp = frequencyInput[chr(v+1)]
+            frequencyInput[chr(v+1)] = temp
+            temp = anotherTemp
+
+        frequencyInput[chr(65)] = temp
+
         if lowDistance > distance(base, frequencyInput):
             lowDistance = distance(base, frequencyInput)
             isBestReversed = True
@@ -44,21 +51,21 @@ def decode(fileName: str):
         lineResult = ""
         for char in line:
             if char.isalpha():
+                char = char.upper()
                 if isBestReversed:
                     if ord(char) - bestRotBy < 65:
                         lineResult += chr(90 - (65 - (ord(char) - bestRotBy)))
                     else:
-                        lineResult += chr(90-bestRotBy)
+                        lineResult += chr(90 - (ord(char)-bestRotBy))
                 else:
                     if ord(char) + bestRotBy > 90:
-                        lineResult += chr(65 + (ord(char) + bestRotBy-90))
+                        lineResult += chr(65 + (ord(char) + bestRotBy - 90))
                     else:
-                        lineResult += chr(65+bestRotBy)
+                        lineResult += chr(ord(char)+bestRotBy)
             else:
                 lineResult += char
 
-            result += lineResult
-            lineResult = ""
+        result += lineResult + "\n"
 
     return result
 
