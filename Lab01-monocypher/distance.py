a11dd02030d752f62770bbf99eccb1eef015b503
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-03-02 23:20:05 -0500

    retooled 1.2 for 1.3 use

--- Lab01-monocypher/distance.py
+++ Lab01-monocypher/distance.py
@@ -4,16 +4,17 @@ from math import sqrt
 import sys
 from frequency import frequency
 
-def distance(file1: str, file2: str):
+def distance(dict1: dict, dict2: dict):
     total = 0
-    file1Freq = frequency(file1)
-    file2Freq = frequency(file2)
-
-    for file1Letter, file2Letter in zip(file1Freq, file2Freq):
-        total += (file1Freq[file1Letter] - file2Freq[file2Letter]) ** 2
-
+    
+    for file1Letter, file2Letter in zip(dict1, dict2):
+        total += (dict1[file1Letter] - dict2[file2Letter]) ** 2
+    
     return sqrt(total)
 
 if __name__ == "__main__":
     if len(sys.argv) >= 3:
-        print(distance(sys.argv[1], sys.argv[2]))
+        file1Freq = frequency(sys.argv[1])
+        file2Freq = frequency(sys.argv[2])
+
+        print(distance(file1Freq, file2Freq))
