3facd0ee6d64f3205f10ba2fea77de0a011d31e9
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-03-12 22:52:59 -0500

    hooked commands up, crack now kinda working

--- Lab02-vigenere/cracker.py
+++ Lab02-vigenere/cracker.py
@@ -1,20 +1,23 @@
 from monocypher import *
+import sys
 
-def guessKeyLength(fileName: str):
+def guessKeyLength(fileName: str, returnDecodedString: bool):
     bestLength = 0
     lowAvgDistance = 2
+    bestBuckets = []
+    textResult = ""
     fileContent = open(fileName, 'r').read()
 
     for i in range(1,17):
-        buckets = []
+        bucket = []
         distance = 0
         for _ in range(i):
-            buckets.append("")
+            bucket.append("")
 
         for v in range(len(fileContent)):
-            buckets[v%i]+= fileContent[v]
+            bucket[v%i]+= fileContent[v]
 
-        for v in buckets:
+        for v in bucket:
             print(v)
             distance += getRotationSpecs(v)["distance"]
             print(decode(v))
@@ -24,10 +27,26 @@ def guessKeyLength(fileName: str):
         if distance < lowAvgDistance:
             lowAvgDistance = distance
             bestLength = i
+            bestBuckets = bucket
         
+    if returnDecodedString:
+        for i in range(len(bucket)):
+            bucket[i] = decode(bucket[i])
         
+        for i in range(len(bucket[0])):
+            textResult += bucket[0][i]
+            if i < len(bucket[1]):
+                textResult += bucket[1][i]
+            if i < len(bucket[2]):
+                textResult += bucket[2][i]
+            if i < len(bucket[3]):
+                textResult += bucket[3][i]
+        
+        return textResult
 
-    print(bestLength)
+    else:
+        return bestLength
 
 if __name__ == "__main__":
-    guessKeyLength("crypt.txt")
+    if len(sys.argv) > 2:
+        print(guessKeyLength(sys.argv[2], sys.argv[1] == "crack"))
