53e7843520888d9c453488fe399039cb73bc3fdc
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-03-24 17:26:53 -0400

    xor component implemented

--- Lab03-/manipulator.py
+++ Lab03-/manipulator.py
@@ -3,16 +3,13 @@
 import sys
 
 def fileManipulator(isDecode: bool, textFilename: str, keyFilename: str, outFilename: str):
-    with open(textFilename, 'rb') as infile, open(outFilename, 'wb') as outfile:
-    # with open(textFilename, 'rb') as infile, open(outFilename, 'wb') as outfile, open(keyFilename, 'rb') as keyFile:
-        changeBy = 1 #TODO: changeby relies on the keyfile
-        if isDecode:
-            changeBy *= -1
+    with open(textFilename, 'rb') as infile, open(outFilename, 'wb') as outfile, open(keyFilename, 'rb') as keyFile:
         fileBytes = infile.read()
+        xorFilter = keyFile.read()
         for i in range(len(fileBytes)):
             b = b'\x05'
             x = int.from_bytes(b, byteorder='big')
-            x = fileBytes[i]+changeBy
+            x = fileBytes[i] ^ xorFilter[i%len(xorFilter)]
             outfile.write(x.to_bytes(1, byteorder='big'))
 
 if __name__ == '__main__':
