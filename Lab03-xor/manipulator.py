d458000ba6f149b5ac5177b7e72c5938409320e2
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-03-28 09:59:17 -0400

    adjusted the input/output as per mr k

--- Lab03-xor/manipulator.py
+++ Lab03-xor/manipulator.py
@@ -2,16 +2,31 @@
 
 import sys
 
-def fileManipulator(isDecode: bool, textFilename: str, keyFilename: str, outFilename: str):
-    with open(textFilename, 'rb') as infile, open(outFilename, 'wb') as outfile, open(keyFilename, 'rb') as keyFile:
+def fileManipulator(textFilename: str, keyFilename: str):
+    result = []
+
+    with open(textFilename, 'rb') as infile, open(keyFilename, 'rb') as keyFile:
         fileBytes = infile.read()
         xorFilter = keyFile.read()
         for i in range(len(fileBytes)):
             b = b'\x05'
             x = int.from_bytes(b, byteorder='big')
             x = fileBytes[i] ^ xorFilter[i%len(xorFilter)]
-            outfile.write(x.to_bytes(1, byteorder='big'))
+            result.append(x.to_bytes(1, byteorder='big'))
+    
+    return result
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        fileManipulator(sys.argv[1] == 'decode', sys.argv[2], sys.argv[3], sys.argv[4])
+        if sys.argv[1] == 'encode':
+            with open(sys.argv[4], 'wb') as outfile:
+                for i in fileManipulator(sys.argv[2], sys.argv[3]):
+                    outfile.write(i)
+        elif sys.argv[1] == 'decode':
+            result = ''
+            for i in fileManipulator(sys.argv[2], sys.argv[3]):
+                b = i
+                x = int.from_bytes(b, byteorder='big')
+                result += chr(x)
+            
+            print(result)
